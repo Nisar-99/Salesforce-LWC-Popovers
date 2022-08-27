e33b9534971bba48a7797df9639e937ff16b9a09
arieend <arieend@gmail.com>
2022-08-27 13:02:19 +0300

    Improve using proxy handler

--- popovers/constants.js
+++ popovers/constants.js
@@ -1,12 +1,15 @@
 
 const SLDS_POPOVER_CLASS = "slds-popover";
+
+const handler = {
+    get: (target, prop) => target[prop.toUpperCase()]
+  }
+
 const POPOVER_SIZES = new Proxy({
   SMALL  : "slds-popover_small",
   MEDIUM : "slds-popover_medium",
   LARGE  : "slds-popover_large"
-}, {
-    get: (target, prop) => target[prop.toUpperCase()]
-});
+}, handler );
 
 const VARIANTS = new Proxy ({
   WARNING : "slds-popover_warning",
@@ -15,9 +18,7 @@ const VARIANTS = new Proxy ({
   SUCCESS : "slds-popover_success",
   WALK    : "slds-popover_walkthrough",
   WALKALT : "slds-popover_walkthrough slds-popover_walkthrough-alt" 
-}, {
-    get: (target, prop) => target[prop.toUpperCase()]
-});
+}, handler );
 
 const PLACEMENT = new Proxy ({
     AUTO   : "auto",
@@ -25,42 +26,37 @@ const PLACEMENT = new Proxy ({
     LEFT   : "left",
     RIGHT  : "right",
     BOTTOM : "bottom"
-}, {
-    get: (target, prop) => target[prop.toUpperCase()]
-});
+}, handler );
+
 const RELATIONS = new Proxy ({
     TOP    : "bottom",
     LEFT   : "right",
     RIGHT  : "left",
     BOTTOM : "top"
-}, {
-    get: (target, prop) => target[prop.toUpperCase()]
-});
+}, handler );
+
 const NUBBIN_PLACEMENT = new Proxy ({
     TOP     : `slds-nubbin--${RELATIONS.TOP}`,
     RIGHT   : `slds-nubbin--${RELATIONS.RIGHT}`,
     LEFT    : `slds-nubbin--${RELATIONS.LEFT}`,
     BOTTOM  : `slds-nubbin--${RELATIONS.BOTTOM}`,
     DEFAULT : `slds-nubbin--${RELATIONS.TOP}`
-}, {
-    get: (target, prop) => target[prop.toUpperCase()]
-});
+}, handler );
+
 const NUBBIN_ADJUSTMENT = new Proxy ({
     TOP     : `nubbin-adjustment--${RELATIONS.TOP}`,
     RIGHT   : `nubbin-adjustment--${RELATIONS.RIGHT}`,
     LEFT    : `nubbin-adjustmnet--${RELATIONS.LEFT}`,
     BOTTOM  : `nubbin-adjustmnet--${RELATIONS.BOTTOM}`
-}, {
-    get: (target, prop) => target[prop.toUpperCase()]
-});
+}, handler );
+
 const NUBBIN_ADJUSTMENT_VARS = new Proxy ({
     TOP     : `--adjustment-${RELATIONS.TOP}`,
     RIGHT   : `--adjustment-${RELATIONS.RIGHT}`,
     LEFT    : `--adjustmnet-${RELATIONS.LEFT}`,
     BOTTOM  : `--adjustmnet-${RELATIONS.BOTTOM}`
-}, {
-    get: (target, prop) => target[prop.toUpperCase()]
-});
+}, handler );
+
 const POPOVER_TOGGGLE = { 
     true: 'popover-show',
     false: 'popover-hide'
@@ -70,9 +66,7 @@ const CALC_FUNCTION = new Proxy ({
     RIGHT  : 'calcHorizontal',
     LEFT   : 'calcHorizontal',
     BOTTOM : 'calcVertical',
-}, {
-    get: (target, prop) => target[prop.toUpperCase()]
-});
+}, handler );
 
 export { 
     SLDS_POPOVER_CLASS, 
